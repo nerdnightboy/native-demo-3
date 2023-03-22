87efc2f5cf5e8763ad0919b5be5ae8361b9021c8
nerdnightboy <nerdnightboy@gmail.com>
2023-03-22 21:19:00 +0530

    demo 3 completed

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button, Animated } from 'react-native';
 
 export default function App() {
+  
+  // const Fade = new Animated.Value(0);
+  // const Spin = new Animated.Value(0);
+  const Spring = new Animated.Value(0);
+  // const ScaleValue = Scale.interpolate({
+  //   inputRange:[0,1],
+  //   outputRange:[0,1]
+  // })
+
+  useEffect(()=>{
+    Animated.spring(Spring,{
+      toValue:1,
+      // duration:5000,
+      friction:1,
+      useNativeDriver:true
+    }).start()
+  },{Spring})
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Animated.View style={{backgroundColor:'black', width:200, height:200, borderRadius:50, transform:[{scale:Spring}] }}>
+    </Animated.View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
